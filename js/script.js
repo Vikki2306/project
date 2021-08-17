bcc9a0f670ca1a8cd74198a0d7f56a52c2f97fdd
Vikki2306 <88272366+Vikki2306@users.noreply.github.com>
2021-08-17 14:47:51 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,33 +1,39 @@
-// Создание числа, которое нужно отгадать:
-const RANDOM_NUMBER = Math.floor(99 * Math.random() + 1); 
-		  
-// Подсчет количества попыток:
-let i = 1;
-		  
-document.getElementById("submitGuess").onclick = function() {
+if (true) {
+	// Создание числа, которое нужно отгадать:
+    const RANDOM_NUMBER = Math.floor(99 * Math.random() + 1);
 
-    let inputText = document.getElementById("guessField").value;
-    let inputNumber = Number(inputText);
-    if (i > 10) {
-        alert('Игра окончена!)');
-	    return;
+    // Подсчет количества попыток:
+    let i = 1;
+		  
+    document.getElementById("submitGuess").onclick = function() {
+	
+        let inputNumber = +document.getElementById("guessField").value;
+
+        if (i > 10) {
+            alert('Игра окончена!');
+	        return;
+        }
+
+        check(inputNumber);
     }
-			 
-	if ((Number.isInteger(inputNumber)) && ((0 < inputNumber) && (inputNumber <= 100) && (inputNumber !== ''))) {
-        if (inputNumber == RANDOM_NUMBER) {
-		    document.getElementById("statements").innerHTML = ('Вы выиграли! Ура!');
-			i = 11; 
-	    } else {
-		    if (inputNumber < RANDOM_NUMBER) {
-				document.getElementById("statements").innerHTML = ('Мое число больше! У вас осталось ' + (10 - i) + ' попыток');
-				i++;
-		    } else {
-				document.getElementById("statements").innerHTML = ('Мое число меньше! У вас осталось ' + (10 - i) + ' попыток');
-				i++;
-		    }  
-	    } 
-	} else {
-		document.getElementById("statements").innerHTML = ('Введите ЧИСЛО от 1 до 100!');
-	}
-}
 
+
+    function check(num) {
+        if ((Number.isInteger(num)) && ((0 < num) && (num <= 100)) && (num !== '')) {	
+            if (num == RANDOM_NUMBER) {
+		        document.getElementById("statements").innerHTML = ('Вы выиграли! Ура!');
+                i = Infinity;		
+	        } else {
+		        if (num < RANDOM_NUMBER) {
+		            document.getElementById("statements").innerHTML = ('Мое число больше! У вас осталось ' + (10 - i) + ' попыток');
+			        i++;
+		        } else {
+			        document.getElementById("statements").innerHTML = ('Мое число меньше! У вас осталось ' + (10 - i) + ' попыток');
+			        i++;
+		        }  
+	        } 
+	    } else {
+		    document.getElementById("statements").innerHTML = ('Введите число от 1 до 100');
+        }
+    }
+}
